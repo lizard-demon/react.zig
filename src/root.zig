35bf720e94c8b8f29ec8a645c485e9bc6eb03890
Your Name <you@example.com>
2026-04-09 17:53:44 -0400

    d

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 pub fn Signals(comptime spec: type) type {
+
     // Extract Fields
     const State = blk: {
         const S = std.meta.fields(spec);
@@ -7,6 +8,8 @@ pub fn Signals(comptime spec: type) type {
         var fields: [S.len + D.len]std.builtin.Type.StructField = undefined;
         for (S, 0..) |f, i| fields[i] = f;
         for (D, 0..) |d, i| {
+            if (@typeInfo(@TypeOf(@field(spec, d.name))) != .@"fn")
+                @compileError("'" ++ d.name ++ "' - Do not declare values.");
             const T = @typeInfo(@TypeOf(@field(spec, d.name))).@"fn".return_type.?;
             fields[S.len + i] = .{
                 .name = d.name,
@@ -17,28 +20,34 @@ pub fn Signals(comptime spec: type) type {
         }
         break :blk @Type(.{ .@"struct" = .{ .layout = .auto, .fields = &fields, .decls = &.{}, .is_tuple = false } });
     };
+
     // Generate Field Enum
     const Tag = std.meta.FieldEnum(State);
+
     // Create DAG
     const Graph = blk: {
         const fields = std.meta.fields(State);
         const decls = std.meta.declarations(spec);
         @setEvalBranchQuota(1000 + (fields.len * fields.len * decls.len * 10));
+
         // Bitmask Map
         const Mask = std.StaticBitSet(fields.len);
         var direct = [_]Mask{Mask.initEmpty()} ** fields.len;
         var reach = [_]Mask{Mask.initEmpty()} ** fields.len;
+
         // Corrilate function paramiters with fields
         for (decls) |decl| {
             const idx = std.meta.fieldIndex(State, decl.name).?;
             const Params = @typeInfo(@TypeOf(@field(spec, decl.name))).@"fn".params[0].type.?;
             inline for (std.meta.fields(Params)) |p| direct[idx].set(std.meta.fieldIndex(State, p.name).?);
         }
+
         // Marks non-function fields as "visited"
         var visited = Mask.initEmpty();
         inline for (std.meta.fields(spec)) |f| {
             visited.set(std.meta.fieldIndex(State, f.name).?);
         }
+
         // Create DAG, recursively traversing from "visited" fields.
         var order: [decls.len]usize = undefined;
         for (0..decls.len) |i| {
@@ -57,12 +66,14 @@ pub fn Signals(comptime spec: type) type {
         }
         break :blk .{ .Mask = Mask, .direct = direct, .reach = reach, .order = order };
     };
+
     // Return -> Graph + API
     return struct {
         const Self = @This();
         pub const Mask = Graph.Mask;
         state: State = .{},
         dirty: Mask = Mask.initEmpty(),
+
         // Field Access
         pub fn set(self: *Self, comptime tag: Tag, val: std.meta.fieldInfo(State, tag).type) void {
             if (std.meta.eql(@field(self.state, @tagName(tag)), val)) return;
@@ -72,6 +83,7 @@ pub fn Signals(comptime spec: type) type {
         pub fn get(self: *const Self, comptime tag: Tag) std.meta.fieldInfo(State, tag).type {
             return @field(self.state, @tagName(tag));
         }
+
         // Resolve Dirty Graph
         pub fn flush(self: *Self) Mask {
             if (self.dirty.count() == 0) return self.dirty;
@@ -92,6 +104,7 @@ pub fn Signals(comptime spec: type) type {
             self.dirty = Mask.initEmpty();
             return out;
         }
+
         // Calculate transitive deps of field at comptime
         pub fn watch(comptime viewed: []const Tag) Mask {
             var m = Mask.initEmpty();
